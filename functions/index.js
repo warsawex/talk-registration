6652397331f0d3be38ea28f915c793e5a4dc3a4a
Kamil Kowalski <kowalski.tychy@gmail.com>
2019-06-09 22:44:04 +0200

    Implement talk submission

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,41 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.submitTalk = functions.https.onRequest(async (req, res) => {
+  const submissionParams = ["description", "email", "language", "name", "title"];
+  const validLanguages = ["pl", "en"];
+
+  let submission = {};
+  let errors = [];
+
+  // Presence validation
+  submissionParams.forEach(param => {
+    if(req.body[param]) {
+      submission[param] = req.body[param];
+    } else {
+      errors.push({ code: "missingParam", field: param });
+    }
+  });
+
+  // Language validation
+  if(submission.language && !validLanguages.includes(submission.language)) {
+    errors.push({ code: "incorrectLanguage", field: "language" });
+  }
+
+  // Email validation
+  if(submission.email && !/.+@.+\..+/.test(submission.email)) {
+    errors.push({ code: "invalidEmail", field: "email" });
+  }
+
+  submission.verified = false;
+  submission.date = admin.database.ServerValue.TIMESTAMP;
+
+  if(errors.length) {
+    res.status(400).send({ errors: errors });
+  } else {
+    await admin.database().ref('/submissions').push(submission);
+    res.status(201).end();
+  }
+});
