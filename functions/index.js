6718e182e0a93bff188ddb34a621c8d811c9e298
Kamil Kowalski <kowalski.tychy@gmail.com>
2019-06-16 14:10:58 +0200

    Add CORS configuration to submission endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,54 +1,57 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const sendgrid = require("@sendgrid/mail");
+const cors = require("cors")({ origin: true });
 
 admin.initializeApp();
 sendgrid.setApiKey(functions.config().sendgrid.apikey);
 
 exports.submitTalk = functions.https.onRequest(async (req, res) => {
-  const submissionParams = [
-    "description",
-    "email",
-    "language",
-    "name",
-    "title"
-  ];
-  const validLanguages = ["pl", "en"];
+  return cors(req, res, async () => {
+    const submissionParams = [
+      "description",
+      "email",
+      "language",
+      "name",
+      "title"
+    ];
+    const validLanguages = ["pl", "en"];
 
-  let submission = {};
-  let errors = [];
+    let submission = {};
+    let errors = [];
 
-  // Presence validation
-  submissionParams.forEach(param => {
-    if (req.body[param]) {
-      submission[param] = req.body[param];
+    // Presence validation
+    submissionParams.forEach(param => {
+      if (req.body[param]) {
+        submission[param] = req.body[param];
+      } else {
+        errors.push({ code: "missingParam", field: param });
+      }
+    });
+
+    // Language validation
+    if (submission.language && !validLanguages.includes(submission.language)) {
+      errors.push({ code: "incorrectLanguage", field: "language" });
+    }
+
+    // Email validation
+    if (submission.email && !/.+@.+\..+/.test(submission.email)) {
+      errors.push({ code: "invalidEmail", field: "email" });
+    }
+
+    submission.verified = false;
+    submission.date = admin.database.ServerValue.TIMESTAMP;
+
+    if (errors.length) {
+      res.status(400).send({ errors: errors });
     } else {
-      errors.push({ code: "missingParam", field: param });
+      await admin
+        .database()
+        .ref("/submissions")
+        .push(submission);
+      res.status(201).end();
     }
   });
-
-  // Language validation
-  if (submission.language && !validLanguages.includes(submission.language)) {
-    errors.push({ code: "incorrectLanguage", field: "language" });
-  }
-
-  // Email validation
-  if (submission.email && !/.+@.+\..+/.test(submission.email)) {
-    errors.push({ code: "invalidEmail", field: "email" });
-  }
-
-  submission.verified = false;
-  submission.date = admin.database.ServerValue.TIMESTAMP;
-
-  if (errors.length) {
-    res.status(400).send({ errors: errors });
-  } else {
-    await admin
-      .database()
-      .ref("/submissions")
-      .push(submission);
-    res.status(201).end();
-  }
 });
 
 exports.sendVerification = functions.database
